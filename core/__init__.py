83502ee10bfbc6138596c50ef54db26902923070
Raghav Mukherjee <mukherjeeraghav05@gmail.com>
2026-03-12 21:42:02 +0530

    Reorder imports in __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,6 +1,5 @@
 from .pdf_parser import PDFParser
 from .preprocessing import TextPreprocessor
-from .sections import split_sections
 from .similarity import SimilarityEngine
 from .skills import SkillExtractor
 from .gap_analysis import GapAnalyzer
